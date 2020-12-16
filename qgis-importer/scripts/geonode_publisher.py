f85bddb04c86f1ed9e0999ae745f8193ad1fd1cc
Mattia Giupponi <mattia.giupponi@gmail.com>
2020-12-16 12:55:09 +0100

    Resolves #33: improve script skeleton

--- qgis-importer/scripts/geonode_publisher.py
+++ qgis-importer/scripts/geonode_publisher.py
@@ -28,7 +28,7 @@ class GeoNodePublisher(QgsProcessingAlgorithm):
         self.addParameter(
              QgsProcessingParameterString('COMMAND', 'Command to execute on GeoNode', multiLine=False, defaultValue=''))
         self.addParameter(
-             QgsProcessingParameterString('GEONODE_USER', 'GeoNode API user', multiLine=False, defaultValue='admin'))
+             QgsProcessingParameterString('GEONODE_USERNAME', 'GeoNode API user', multiLine=False, defaultValue='admin'))
         self.addParameter(
              QgsProcessingParameterString('GEONODE_PASSWORD', 'GeoNode API password', multiLine=False, defaultValue='geonode'))
         self.addParameter(
@@ -38,7 +38,7 @@ class GeoNodePublisher(QgsProcessingAlgorithm):
         self.addParameter(
             QgsProcessingParameterString('GS_PASSWORD', 'GS Admin password', multiLine=False, defaultValue='geoserver'))
         self.addParameter(
-            QgsProcessingParameterString('GS_STORE', 'GS Datastore Name', multiLine=False, defaultValue=None))
+            QgsProcessingParameterString('GS_STORE_NAME', 'GS Datastore Name', multiLine=False, defaultValue=None))
         self.addParameter(
             QgsProcessingParameterString('GS_WORKSPACE', 'GS Workspace Name', multiLine=False, defaultValue=None))
     # def get_db_params(self, db_name):
@@ -68,20 +68,152 @@ class GeoNodePublisher(QgsProcessingAlgorithm):
         feedback.pushInfo("Get GeoServer Catalog: " + parameters["GS_REST_URL"])
 
         layers = self.fetch_layers_from_geoserver(parameters)
-
         feedback.pushInfo(f"The following layers are sent to Geonode: {layers}")
 
         auth = HTTPBasicAuth(parameters['GEONODE_USERNAME'], parameters['GEONODE_PASSWORD'])
+        output = []
         for layer in layers:
+
             # TODO to be defined the json to sent to the API
             result = requests.post(url=parameters['GEONODE_REST_URL'], auth=auth)
             if result.status_code == 200:
                 feedback.pushInfo(f"Request for layer {layer} successfuly sent")
+                output.append(layer.name)
             else:
                 feedback.pushInfo(f"Error during processing request for layer {layer}")
 
         feedback.pushInfo("Layers processing completed")
-        return {}
+        return output
+
+    @staticmethod
+    def fetch_layers_from_geoserver(parameters):
+        store_name = parameters['GS_STORE_NAME']
+        workspace = parameters['GS_WORKSPACE']
+        gs_catalogue = Catalog(parameters["GS_REST_URL"], parameters["GS_ADMIN"], parameters["GS_PASSWORD"])
+
+        store = gs_catalogue.get_store(store_name, workspace)
+
+        return gs_catalogue.get_layers(store)
+
+    def name(self):
+        """
+        Name of the algorithm
+        :return:
+        """
+        return 'GeoNodePublisher'
+
+    def displayName(self):
+        """
+        Name to display for the algorithm in QGIS
+        :return:
+        """
+        return 'GeoNode Publisher'
+
+    def group(self):
+        """
+        Name of the group for this script
+        :return:
+        """
+        return 'krihs'
+
+    def groupId(self):
+        """
+        Identifier for the group
+        """
+        return 'krihs'
+
+    def createInstance(self):
+        """
+        Create the algorithm instance
+        :return:
+        """
+        return GeoNodePublisher()
+import os
+from qgis.core import *
+from geoserver.catalog import Catalog
+import xml.dom.minidom
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class GeoNodePublisher(QgsProcessingAlgorithm):
+    """
+    Algorithm to define and import feature classes from geopackage and EXRI Xml Workspace definition into PostGIS
+    """
+
+    def initAlgorithm(self, config=None):
+        """
+        Initialize the algorithm.
+        It accepts the following parameters (via QGIS processing):
+        - GEONODE_REST_URL: GeoServer ReST url (default 'http://localhost:8080/api/v2/management/')
+        - COMMAND: name of command to execute on GeoNode
+        - GEONODE_USER: GeoNode username (default 'admin')
+        - GEONODE_PASSWORD: GeoNode username password (default 'geoserver')
+        """
+        self.addParameter(
+             QgsProcessingParameterString('GEONODE_REST_URL', 'GeoNode REST address', multiLine=False, defaultValue='http://localhost:8080/api/v2/management/'))
+        self.addParameter(
+            QgsProcessingParameterString('GS_REST_URL', 'GS ReST address', multiLine=False,
+                                         defaultValue='http://localhost:8080/geoserver/rest/'))
+        self.addParameter(
+             QgsProcessingParameterString('COMMAND', 'Command to execute on GeoNode', multiLine=False, defaultValue=''))
+        self.addParameter(
+             QgsProcessingParameterString('GEONODE_USERNAME', 'GeoNode API user', multiLine=False, defaultValue='admin'))
+        self.addParameter(
+             QgsProcessingParameterString('GEONODE_PASSWORD', 'GeoNode API password', multiLine=False, defaultValue='geonode'))
+        self.addParameter(
+            QgsProcessingParameterString('DB_NAME', 'Pg Connection Name', multiLine=False, defaultValue='KRIHS'))
+        self.addParameter(
+            QgsProcessingParameterString('GS_ADMIN', 'GS Admin user', multiLine=False, defaultValue='admin'))
+        self.addParameter(
+            QgsProcessingParameterString('GS_PASSWORD', 'GS Admin password', multiLine=False, defaultValue='geoserver'))
+        self.addParameter(
+            QgsProcessingParameterString('GS_STORE_NAME', 'GS Datastore Name', multiLine=False, defaultValue=None))
+        self.addParameter(
+            QgsProcessingParameterString('GS_WORKSPACE', 'GS Workspace Name', multiLine=False, defaultValue=None))
+    # def get_db_params(self, db_name):
+    #     qs = QgsSettings()
+    #     qs_pg_prefix = "PostgreSQL/connections/" + db_name + "/"
+    #     return dict(
+    #         host=qs.value(qs_pg_prefix + "host"),
+    #         port=qs.value(qs_pg_prefix + "port"),
+    #         dbtype="postgis",
+    #         database=qs.value(qs_pg_prefix + "database"),
+    #         user=qs.value(qs_pg_prefix + "username"),
+    #         passwd=qs.value(qs_pg_prefix + "password"),
+    #     )
+
+    def processAlgorithm(self, parameters, context, model_feedback):
+        """
+        Process the algorithm
+        :param parameters: parameters of the process
+        :param context: context of the process
+        :param model_feedback: feedback instance for the process
+        :return:
+        """
+        # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
+        # overall progress through the model
+
+        feedback = QgsProcessingMultiStepFeedback(0, model_feedback)
+        feedback.pushInfo("Get GeoServer Catalog: " + parameters["GS_REST_URL"])
+
+        layers = self.fetch_layers_from_geoserver(parameters)
+        feedback.pushInfo(f"The following layers are sent to Geonode: {layers}")
+
+        auth = HTTPBasicAuth(parameters['GEONODE_USERNAME'], parameters['GEONODE_PASSWORD'])
+        output = []
+        for layer in layers:
+
+            # TODO to be defined the json to sent to the API
+            result = requests.post(url=parameters['GEONODE_REST_URL'], auth=auth)
+            if result.status_code == 200:
+                feedback.pushInfo(f"Request for layer {layer} successfuly sent")
+                output.append(layer.name)
+            else:
+                feedback.pushInfo(f"Error during processing request for layer {layer}")
+
+        feedback.pushInfo("Layers processing completed")
+        return output
 
     @staticmethod
     def fetch_layers_from_geoserver(parameters):
--- qgis-importer/tests/test_geonode_publisher.py
+++ qgis-importer/tests/test_geonode_publisher.py
@@ -1,14 +1,33 @@
 import unittest
 import sys
+from collections import namedtuple
+from unittest.mock import MagicMock, patch
 
 sys.path.append('C:\\OSGeo4W64\\apps\\qgis\\python\\plugins')
 
-from scripts.geonode_publisher import GeoNodePublisher
+from scripts.geonode_publisher import GeoNodePublisher, QgsProcessingFeedback
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.sut = GeoNodePublisher()
+        self.example_layer = namedtuple("Layer", ["title"])
+        self.example_layer.name = "my_layer_title"
+        self.successful_request = namedtuple("Response", ["status_code"])
+        self.successful_request.status_code = 200
+        self.parameters = {
+            'GEONODE_REST_URL': 'http://localhost:8080/api/v2/management/',
+            'GS_REST_URL': 'http://localhost:8080/geoserver/rest/',
+            'COMMAND': '',
+            'GEONODE_USERNAME': 'admin',
+            'GEONODE_PASSWORD': 'geonode',
+            'DB_NAME': 'KRIHS',
+            'GS_ADMIN': 'admin',
+            'GS_PASSWORD': 'geoserver',
+            'GS_STORE_NAME': None,
+            'GS_WORKSPACE': None
+        }
+        self.sut.fetch_layers_from_geoserver = MagicMock()
 
     def test_sut_correclty_initiate(self):
         self.assertEqual(GeoNodePublisher, type(self.sut.createInstance()))
@@ -25,10 +44,13 @@ class MyTestCase(unittest.TestCase):
     def test_sut_name_is_the_expected_one(self):
         self.assertEqual("GeoNodePublisher", self.sut.name())
 
-    def test_sut_processAlgorithm_should_produce_the_expected_output(self):
-        x = self.sut.processAlgorithm()
-        print(x)
-        self.assertTrue(False)
+    @patch("requests.post")
+    def test_sut_processAlgorithm_should_produce_the_expected_output(self, mocked_post_request):
+        mocked_post_request.return_value = self.successful_request
+        self.sut.fetch_layers_from_geoserver.return_value = [self.example_layer]
+        expected = [self.example_layer.name]
+        actual = self.sut.processAlgorithm(self.parameters, {}, QgsProcessingFeedback())
+        self.assertListEqual(expected, actual)
 
 
 if __name__ == '__main__':
